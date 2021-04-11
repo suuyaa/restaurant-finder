e9470481277970045931a385d945bf8a288a92d5
Suya Tang <tangsygo@gmail.com>
2021-04-11 17:31:15 -0400

    deploy database

--- server/db.sql
+++ server/db.sql
@@ -13,7 +13,7 @@ CREATE TABLE restaurants (
 	id BIGSERIAL NOT NULL UNIQUE,
 	name VARCHAR(50) NOT NULL,
 	location VARCHAR(50) NOT NULL,
-	price_range INT	NOT NULL
+	price_range INT	 NOT NULL,
 );
 	
 
