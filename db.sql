c6600dca4402a0b953d6c07f8a56241fdd70999f
Suya Tang <tangsygo@gmail.com>
2021-04-11 21:41:23 -0400

    database update

--- db.sql
+++ db.sql
@@ -1,3 +1,12 @@
+
+
+CREATE TABLE restaurants (
+	id BIGSERIAL NOT NULL UNIQUE,
+	name VARCHAR(50) NOT NULL,
+	location VARCHAR(50) NOT NULL,
+	price_range INT	 NOT NULL
+);
+	
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
@@ -8,15 +17,3 @@ CREATE TABLE reviews (
         and rating <= 5
     )
 );
-
-CREATE TABLE restaurants (
-	id BIGSERIAL NOT NULL UNIQUE,
-	name VARCHAR(50) NOT NULL,
-	location VARCHAR(50) NOT NULL,
-	price_range INT	 NOT NULL
-);
-	
-
-INSERT INTO restaurants (name, location, price_range) values ('mcdonalds', 'toronto', 3);
-
-SELECT * FROM restaurants;
--- db/index.js
+++ db/index.js
@@ -5,9 +5,13 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 
 const proConfig = process.env.DATABASE_URL;
 
+let connectionString =
+  process.env.NODE_ENV === "production" ? proConfig : devConfig;
+
+console.log("DBURL:", connectionString);
+
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  connectionString: connectionString,
 });
 
 module.exports = {
